301655f8552d72f727eef58bfccbf8c556b99b9c
Ankur Singh <ramanujanankur@gmail.com>
2014-12-18 21:32:25 +0530

    Homework Done

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,27 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+#This function creates a special matrix object(here list of 2 matrices)
 makeCacheMatrix <- function(x = matrix()) {
-
+       if(is.null(x))print("Null matrix provided")
+       #assuming matrix is invertible
+       cachematrix <- solve(x)
+       invisible(list(x, cachematrix))
 }
 
 
 ## Write a short comment describing this function
-
+#This function finds the inverse if not provided by "special matrix"
 cacheSolve <- function(x, ...) {
+        speMatrix<-makeCacheMatrix(x)
+        if(is.null(speMatrix$cachmatrix)){
+          cached<-solve(x)
+        }
+        #if failed means inverse not calculated
+        #calculating inverse in else loop
+        else{
+          cached<-specMatrix$cachematrix
+        }
+        cached
         ## Return a matrix that is the inverse of 'x'
 }
